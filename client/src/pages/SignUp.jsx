4a2c0d320bc53daada8bc1b8be26986b2bff37c6
Muhammad Usman <musman.akhtar21@gmail.com>
2024-07-26 19:49:16 +0500

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -76,7 +76,7 @@ const SignUp = () => {
 				<button
 					disabled={loading}
 					className="bg-slate-700 text-white p-3 rounded-lg 
-          uppercase hover:opacity-95 disabled:opacity-80">
+        			uppercase hover:opacity-95 disabled:opacity-80">
 					{loading ? "Loading..." : "Sign Up"}
 				</button>
 			</form>
