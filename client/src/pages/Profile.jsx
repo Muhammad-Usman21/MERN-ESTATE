89ca5d8c1f372374b61961e6650d9989a7d5843c
Muhammad Usman <musman.akhtar21@gmail.com>
2024-08-01 16:54:56 +0500

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
 	signOutUserSuccess,
 	signOutUserFailure,
 } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
 	const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -137,65 +138,74 @@ const Profile = () => {
 	return (
 		<div className="p-3 max-w-lg mx-auto">
 			<h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-			<form onSubmit={handleSubmit} className="flex flex-col gap-4">
-				<input
-					onChange={(e) => setFile(e.target.files[0])}
-					type="file"
-					ref={fileRef}
-					hidden
-					accept="image/*"
-				/>
-				<img
-					onClick={() => fileRef.current.click()}
-					src={formData.avatar || currentUser.avatar}
-					alt="profile"
-					className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-				/>
-				<p className="text-sm self-center">
-					{fileUploadError ? (
-						<span className="text-red-700">
-							Error image upload (Image must be less than 2 MBs)
-						</span>
-					) : filePercentage > 0 && filePercentage < 100 ? (
-						<span className="text-slate-700">
-							{`Uploading ${filePercentage}%`}
-						</span>
-					) : filePercentage === 100 && !fileUploadError ? (
-						<span className="text-green-700">Image successfully uploaded!</span>
-					) : (
-						""
-					)}
-				</p>
-				<input
-					type="text"
-					placeholder="username"
-					defaultValue={currentUser.username}
-					className="border p-3 rounded-lg"
-					id="username"
-					onChange={handleChange}
-				/>
-				<input
-					type="email"
-					placeholder="email"
-					defaultValue={currentUser.email}
-					className="border p-3 rounded-lg"
-					id="email"
-					onChange={handleChange}
-				/>
-				<input
-					type="password"
-					placeholder="password"
-					className="border p-3 rounded-lg"
-					id="password"
-					onChange={handleChange}
-				/>
-				<button
-					disabled={loading}
-					className="bg-slate-700 text-white rounded-lg p-3 uppercase 
+			<div className="flex flex-col gap-4">
+				<form onSubmit={handleSubmit} className="flex flex-col gap-4">
+					<input
+						onChange={(e) => setFile(e.target.files[0])}
+						type="file"
+						ref={fileRef}
+						hidden
+						accept="image/*"
+					/>
+					<img
+						onClick={() => fileRef.current.click()}
+						src={formData.avatar || currentUser.avatar}
+						alt="profile"
+						className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+					/>
+					<p className="text-sm self-center">
+						{fileUploadError ? (
+							<span className="text-red-700">
+								Error image upload (Image must be less than 2 MBs)
+							</span>
+						) : filePercentage > 0 && filePercentage < 100 ? (
+							<span className="text-slate-700">
+								{`Uploading ${filePercentage}%`}
+							</span>
+						) : filePercentage === 100 && !fileUploadError ? (
+							<span className="text-green-700">
+								Image successfully uploaded!
+							</span>
+						) : (
+							""
+						)}
+					</p>
+					<input
+						type="text"
+						placeholder="username"
+						defaultValue={currentUser.username}
+						className="border p-3 rounded-lg"
+						id="username"
+						onChange={handleChange}
+					/>
+					<input
+						type="email"
+						placeholder="email"
+						defaultValue={currentUser.email}
+						className="border p-3 rounded-lg"
+						id="email"
+						onChange={handleChange}
+					/>
+					<input
+						type="password"
+						placeholder="password"
+						className="border p-3 rounded-lg"
+						id="password"
+						onChange={handleChange}
+					/>
+					<button
+						disabled={loading}
+						className="bg-slate-700 text-white rounded-lg p-3 uppercase 
           			hover:opacity-95 disabled:opacity-80">
-					{loading ? "Loading..." : "Update"}
-				</button>
-			</form>
+						{loading ? "Loading..." : "Update"}
+					</button>
+				</form>
+				<Link
+					className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+					to={"/create-listing"}>
+					Create Listing
+				</Link>
+			</div>
 			<div className="flex justify-between mt-5">
 				<span
 					onClick={handleDeleteUser}
